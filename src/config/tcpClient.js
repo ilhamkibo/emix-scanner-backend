871ad28835f7cc71ff27a0be9f9f61a574b5181a
ilhamkibo <ilham.ilam59@gmail.com>
2025-02-25 17:22:26 +0700

    Update API

--- src/config/tcpClient.js
+++ src/config/tcpClient.js
@@ -2,31 +2,36 @@ const net = require("net");
 
 const TCP_PORT = process.env.TCP_PORT || 4001; // Port untuk timbangan
 const TCP_HOST = process.env.TCP_HOST || "192.168.245.95"; // IP timbangan
+const RECONNECT_INTERVAL = 5000; // Interval reconnect dalam milidetik
 
 let tcpClient;
 
 const createTCPClient = (onDataCallback) => {
-  tcpClient = new net.Socket();
+  const connectToServer = () => {
+    tcpClient = new net.Socket();
 
-  tcpClient.connect(TCP_PORT, TCP_HOST, () => {
-    console.log(`Connected to TCP server at ${TCP_HOST}:${TCP_PORT}`);
-  });
+    tcpClient.connect(TCP_PORT, TCP_HOST, () => {
+      console.log(`Connected to TCP server at ${TCP_HOST}:${TCP_PORT}`);
+    });
 
-  tcpClient.on("data", (data) => {
-    const weightValue = data.toString().trim(); // Parsing data dari timbangan
-    if (onDataCallback) {
-      onDataCallback(weightValue); // Kirim data berat ke callback
-    }
-  });
+    tcpClient.on("data", (data) => {
+      const weightValue = data.toString().trim(); // Parsing data dari timbangan
+      if (onDataCallback) {
+        onDataCallback(weightValue); // Kirim data berat ke callback
+      }
+    });
 
-  tcpClient.on("error", (err) => {
-    console.error("TCP Client Error:", err.message);
-  });
+    tcpClient.on("error", (err) => {
+      console.error("TCP Client Error:", err.message);
+    });
 
-  tcpClient.on("close", () => {
-    console.log("TCP connection closed.");
-  });
+    tcpClient.on("close", () => {
+      console.log("TCP connection closed. Attempting to reconnect...");
+      setTimeout(connectToServer, RECONNECT_INTERVAL); // Coba reconnect
+    });
+  };
 
+  connectToServer(); // Inisialisasi koneksi pertama kali
   return tcpClient;
 };
 
